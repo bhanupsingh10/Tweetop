50f42baea0a8cf1ab03c4b714c9ed298ac04a952
guptag911 <17ucc003@lnmiit.ac.in>
2020-10-05 23:04:20 +0530

    Comments Added

--- tweetop/tweet/utils.py
+++ tweetop/tweet/utils.py
@@ -1,13 +1,13 @@
 import tweepy
 
-API_key = "ohUSPDNOE5qc1X063SudPPCy9"
-API_key_secret = "UnQJaovLV1PLBpqCDQaM5oKj6K0d2nEFbGOl11VTPScxmICiEE"
+API_key = "YOUR API KEY"
+API_key_secret = "YOUR API KEY SECRET"
 
-def get_Api(request):
+def get_Api(request):			# Returns our tweepy api handler
 	oauth = tweepy.OAuthHandler(API_key, API_key_secret)
 	token = request.session.get('request_token')
 	oauth.request_token = token
 	oauth.access_token = request.session['access_key']
 	oauth.access_token_secret = request.session['access_key_secret']
-	api = tweepy.API(oauth)
-	return api	
+	api = tweepy.API(oauth)	
+	return api							
