0b3c08d86e4e8a5913a89de04dd6d320f4a45b1c
guptag911 <17ucc003@lnmiit.ac.in>
2020-10-05 23:01:35 +0530

    Comments Added

--- tweetop/users/views.py
+++ tweetop/users/views.py
@@ -8,17 +8,17 @@ API_key_secret = "TWITTER API KEY SECRET"
 
 
 def index_view(request):
-	print(request)
+	#print(request)
 	if(checker(request)):
-		print("Path is: ", request.path)
-		print("Domain is: ", request.get_host())
-		return redirect("http://" + request.get_host() + "/home")
+		#print("Path is: ", request.path)
+		#print("Domain is: ", request.get_host())
+		return redirect("http://" + request.get_host() + "/home") 	# Sending user diretly to home page is already logged In!
 		#return path('home/', dashboard)
 	else:
-		return render_to_response("login.html")
+		return render_to_response("login.html")					# User Not logged In.
 
 
-def checker(request):
+def checker(request):			# Checks wether there is correct secret as cokkies or not
 	try:
 		access_key = request.session.get('access_key', None)
 		if not access_key:
@@ -28,35 +28,35 @@ def checker(request):
 	return True
 
 def auth_view(request):
-	OAuth = tweepy.OAuthHandler(API_key, API_key_secret)
-	redirect_url = OAuth.get_authorization_url()
-	response = HttpResponseRedirect(redirect_url)
+	OAuth = tweepy.OAuthHandler(API_key, API_key_secret)			# InitaliAIng our Oauth1 client
+	redirect_url = OAuth.get_authorization_url()			# URL Generated where PIN Is
+	response = HttpResponseRedirect(redirect_url)	
 	
 	#print(response)
-	request.session['request_token'] = OAuth.request_token
-	print(OAuth.request_token)
+	request.session['request_token'] = OAuth.request_token		# Saving the genrated token
+	#print(OAuth.request_token)
 	#return render("<h1>Hello</h1>")
-	return response
+	return response								#Sending the user to auth URL
 
 def callback(request):
-	pin = request.GET.get('pin')
+	pin = request.GET.get('pin')				# Getting PIN from get request
 	oauth = tweepy.OAuthHandler(API_key, API_key_secret)
-	token = request.session.get('request_token')
+	token = request.session.get('request_token')		# Getting out token 
 	request.session.delete('request_token')
 	oauth.request_token = token
-	print("Token is", token)
+	#print("Token is", token)
 	
 	try:
-		oauth.get_access_token(pin)
-		request.session['access_key'] = oauth.access_token
+		oauth.get_access_token(pin)				
+		request.session['access_key'] = oauth.access_token			# Saving Auth keys for  future use.
 		request.session['access_key_secret'] = oauth.access_token_secret
 		api = tweepy.API(oauth)
-		me = api.me()
-		print(me.screen_name)
+		me = api.me()									# API connected!
+		#print(me.screen_name)
 	except:
 		print("Error Occured at views 43!")
 	
-	return redirect("http://" + request.get_host())
+	return redirect("http://" + request.get_host())			# Sending user to index view.
 #
 # return render(request,'index_view', {})
 	#return render(request,'home.html',{})
